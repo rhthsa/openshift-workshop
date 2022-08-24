b95a8fbfa4d7aa51fd217455da8f3a89900bdb1b
chatapazar <31641737+chatapazar@users.noreply.github.com>
2022-08-24 11:38:25 +0700

    Update setup_user.sh

--- bin/setup_user.sh
+++ bin/setup_user.sh
@@ -60,5 +60,105 @@ oc login -u user29 -p openshift
 oc new-project user29
 oc login -u user30 -p openshift
 oc new-project user30
-
-
+oc login -u user31 -p openshift
+oc new-project user31
+oc login -u user32 -p openshift
+oc new-project user32
+oc login -u user33 -p openshift
+oc new-project user33
+oc login -u user34 -p openshift
+oc new-project user34
+oc login -u user35 -p openshift
+oc new-project user35
+oc login -u user36 -p openshift
+oc new-project user36
+oc login -u user37 -p openshift
+oc new-project user37
+oc login -u user38 -p openshift
+oc new-project user38
+oc login -u user39 -p openshift
+oc new-project user39
+oc login -u user40 -p openshift
+oc new-project user40
+oc login -u user41 -p openshift
+oc new-project user41
+oc login -u user42 -p openshift
+oc new-project user42
+oc login -u user43 -p openshift
+oc new-project user43
+oc login -u user44 -p openshift
+oc new-project user44
+oc login -u user45 -p openshift
+oc new-project user45
+oc login -u user46 -p openshift
+oc new-project user46
+oc login -u user47 -p openshift
+oc new-project user47
+oc login -u user48 -p openshift
+oc new-project user48
+oc login -u user49 -p openshift
+oc new-project user49
+oc login -u user50 -p openshift
+oc new-project user50
+oc login -u user51 -p openshift
+oc new-project user51
+oc login -u user52 -p openshift
+oc new-project user52
+oc login -u user53 -p openshift
+oc new-project user53
+oc login -u user54 -p openshift
+oc new-project user54
+oc login -u user55 -p openshift
+oc new-project user55
+oc login -u user56 -p openshift
+oc new-project user56
+oc login -u user57 -p openshift
+oc new-project user57
+oc login -u user58 -p openshift
+oc new-project user58
+oc login -u user59 -p openshift
+oc new-project user59
+oc login -u user60 -p openshift
+oc new-project user60
+oc login -u user61 -p openshift
+oc new-project user61
+oc login -u user62 -p openshift
+oc new-project user62
+oc login -u user63 -p openshift
+oc new-project user63
+oc login -u user64 -p openshift
+oc new-project user64
+oc login -u user65 -p openshift
+oc new-project user65
+oc login -u user66 -p openshift
+oc new-project user66
+oc login -u user67 -p openshift
+oc new-project user67
+oc login -u user68 -p openshift
+oc new-project user68
+oc login -u user69 -p openshift
+oc new-project user69
+oc login -u user70 -p openshift
+oc new-project user70
+oc login -u user71 -p openshift
+oc new-project user71
+oc login -u user72 -p openshift
+oc new-project user72
+oc login -u user73 -p openshift
+oc new-project user73
+oc login -u user74 -p openshift
+oc new-project user74
+oc login -u user75 -p openshift
+oc new-project user75
+oc login -u user76 -p openshift
+oc new-project user76
+oc login -u user77 -p openshift
+oc new-project user77
+oc login -u user78 -p openshift
+oc new-project user78
+oc login -u user78 -p openshift
+oc new-project user79
+oc login -u user79 -p openshift
+oc new-project user79
+oc login -u user80 -p openshift
+oc new-project user80
